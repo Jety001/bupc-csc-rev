670ea41c16fa05b2027efa853c0d3fa9fbcfeb4b
Jety001 <shin.santiano@gmail.com>
2021-11-17 12:52:10 +0800

    first commit

---
+++ admin/officers/process.php
@@ -0,0 +1,43 @@
+<?php
+
+
+    session_start();
+    include('../conn.php');
+
+    if (isset($_POST['save'])) {
+        $id = $_POST['id'];
+        $name = $_POST['name'];
+        $position = $_POST['position'];
+        $course = $_POST['course'];
+        $yearLevel = $_POST['yearLevel'];
+        $buEmail = $_POST['buEmail'];
+        $fb = $_POST['fb'];
+        $picture = $_FILES["profileImage"]["name"];
+        $target_dir = "./upload/";
+        $target_file = $target_dir . basename($picture);
+        if($_FILES['profileImage']['size'] > 200000) {
+            $msg = "Image size should not be greated than 200Kb";
+            $msg_class = "alert-danger";
+        }
+        if(file_exists($target_file)) {
+            $msg = "File already exists";
+            $msg_class = "alert-danger";
+        }
+        // if the input file has a value
+        if (empty($error)) {
+            if(move_uploaded_file($_FILES["profileImage"]["tmp_name"], $target_file)) {
+                    mysqli_query($db, "UPDATE officers SET picture='$picture' WHERE id=$id");
+            } else {
+                $error = "There was an erro uploading the file";
+                $msg = "alert-danger";
+            }
+        }
+        $query = "UPDATE officers SET name='$name', position='$position', course='$course', yearLevel='$yearLevel', buEmail='$buEmail', fb='$fb' WHERE id=$id";
+        mysqli_query($db, $query);
+        header('location: ../officers/');
+    }
+
+
+
+
+?>
---
+++ login_process.php
@@ -0,0 +1,59 @@
+<?php
+    // one year login
+    // ini_set('session.cookie_lifetime', 60 * 60 * 24 * 365);
+    // ini_set('session.gc-maxlifetime', 60 * 60 * 24 * 365);
+    // end
+    session_start();
+    include "conn.php";
+
+    if(isset($_POST['name']) && isset($_POST['studentId'])) {
+        function validate($data){
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
+
+        $name = validate($_POST['name']);
+        $studentId = validate($_POST['studentId']);
+    
+
+        if (empty($name)) {
+            header("Location: index.php?error=name is required");
+            exit();
+        } else if(empty($studentId)){
+            header("Location: index.php?error=Password is required");
+            exit();
+        }
+        // pag goods su name and pass amo kadi maga execute na code
+        else{
+            $sql = "SELECT * FROM studentsAcc WHERE name='$name' AND studentId='$studentId'";
+
+            $result = mysqli_query($db, $sql);
+            if(mysqli_num_rows($result)) {
+
+                    $row = mysqli_fetch_assoc($result);
+                    $_SESSION['name'] = $row['name'];
+                    $_SESSION['id'] = $row['id'];
+
+                    header('location: index.php');
+                    $_SESSION['status'] = "Woo hoo!";
+                    $_SESSION['img'] = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREdklY1YcgCVMSLmTwiQ8T-2tqvy3gNOLkog&usqp=CAU";
+                    $_SESSION['text'] = "Login successfull!";
+                }
+                    else{
+                    header("Location: index.php?error=name and studentId not match");
+                    $_SESSION['status'] = "Oopss!";
+                    $_SESSION['img'] = "https://assets.website-files.com/5b5f4cc1bb5a8369e423e901/5f6511fb6158287d9188ebb1_ugging_his_shoulders_1200x628_091020.svg";
+                    $_SESSION['text'] = "Please check your inputs";
+                 
+                    exit();
+              }
+            }
+
+    }else{
+        header('location: index.php');
+        exit();
+    }
+
+?>
